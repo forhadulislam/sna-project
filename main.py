eeb9d837b87ff67a9fea281ef825307b8070b6e5
A H M Forhadul Islam <ronex.sadi@yahoo.com>
2017-03-06 21:35:09 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import nltk, numpy
+from nltk.tokenize import sent_tokenize
+
+if __name__ == '__main__':
+
+    # Reading file ydata.txt which contains the data
+    f = open('ydata.txt')
+    rawText = f.read()
+    # Splitting texts into arrays by newline
+    splitText = rawText.split('\n')
+    #tokenized_sentences = sent_tokenize(splitText)
+    for i in range(0,100):
+        text = sent_tokenize(splitText[i])
+        textPosTag = nltk.pos_tag(text)
+        print(textPosTag)
