93e30132af55aad2f36923eb50e3c7555fe7332e
A H M Forhadul Islam <ronex.sadi@yahoo.com>
2017-04-01 16:38:17 +0300

    added arangodb client and semantic_similarity

--- bk.py
+++ bk.py
@@ -2,16 +2,23 @@ import nltk.collocations
 import nltk.corpus
 import collections
 from nltk.corpus import wordnet as wn
+from slugify import slugify
+
 
 f = open('ydata.txt')
 rawText = f.read()
 splitText = rawText.split('\n')
-#bgm    = nltk.collocations.BigramAssocMeasures()
-tgm    = nltk.collocations.TrigramAssocMeasures()
-#finder = nltk.collocations.BigramCollocationFinder.from_words( splitText )
-tFinder = nltk.collocations.TrigramCollocationFinder.from_words( splitText )
-scored = tFinder.score_ngrams( tgm.raw_freq )
+bgm    = nltk.collocations.BigramAssocMeasures()
+#tgm    = nltk.collocations.TrigramAssocMeasures()
+tFinder = nltk.collocations.BigramCollocationFinder.from_words( splitText )
+#tFinder = nltk.collocations.TrigramCollocationFinder.from_words( splitText )
+scored = tFinder.score_ngrams( bgm.raw_freq )
 
+
+    
+    
+
+'''
 # Group bigrams by first word in bigram.                                        
 prefix_keys = collections.defaultdict(list)
 for key, scores in scored:
@@ -21,11 +28,107 @@ for key, scores in scored:
 for key in prefix_keys:
    prefix_keys[key].sort(key = lambda x: -x[1])
 
-'''
+
 print 'www', prefix_keys['www.yahoo,ro'][0:10]
 print 'xmla', prefix_keys['xmla'][:5]
 print 'yooujizz', prefix_keys['yooujizz'][:5]
-'''
 
-print( sorted(tFinder.nbest(tgm.raw_freq, 10)) )
-print( wn.synsets('www.google.com')[0].definition() )
+
+#print( sorted(tFinder.nbest(bgm.raw_freq, 10)) )
+#print( wn.synsets('www.google.com')[0].definition() )
+
+# Frequency of words
+splitByWord = rawText.split()
+Freq_dist_nltk=nltk.FreqDist(splitByWord)
+#print Freq_dist_nltk
+
+#print splitByWord.collocations()
+
+# Showing freq distribution numbers
+for k,v in Freq_dist_nltk.items():
+    if v > 1:
+        #print str(k)+':'+str(v)
+        print str(v)+'    '+str(k)        
+'''     
+def semantic_similarity(str1, str2):
+    
+    tk1 = nltk.word_tokenize(str1)
+    pt1 = nltk.pos_tag(tk1)
+    tk2 = nltk.word_tokenize(str2)
+    pt2 = nltk.pos_tag(tk2)
+    
+    nounWeight = 0
+    verbWeight = 0
+    outputWeight = 0
+    s = 0
+    
+    # Calculating noun weights
+    propernouns1 = [word for word,pos in pt1 if pos == 'NN' or pos == 'NNP' or pos == 'PRP']
+    propernouns2 = [word for word,pos in pt2 if pos == 'NN' or pos == 'NNP' or pos == 'PRP']    
+    
+    if propernouns1 and propernouns2:
+        for pn1 in propernouns1:
+            syns1 = wn.synsets(pn1)
+            for pn2 in propernouns2:
+                d = nltk.edit_distance(pn1, pn2)
+                syns2 = wn.synsets(pn2)
+                if syns1 and syns2:
+                    s = syns1[0].wup_similarity(syns2[0])                    
+                    #s = syns1[0].path_similarity(syns2[0])
+                    if s > 0.2 and s < 0.4:
+                        nounWeight += 0.4
+                    elif s >= 0.4 and s < 0.75:
+                        nounWeight += s
+                    elif s >= 0.75 and s <= 1:
+                        nounWeight += 1
+                    else:
+                        if s:
+                            nounWeight += s
+                
+                    # If distance is lesst than or equal 2 then add 0.2
+                    if d <= 2:  
+                        nounWeight += 0.2
+                
+    # Calculating verb weights
+    verbs1 = [word for word,pos in pt1 if pos == 'VBN' or pos == 'VB']
+    verbs2 = [word for word,pos in pt2 if pos == 'VBN' or pos == 'VB']    
+    s = 0
+    if verbs1 and verbs2:
+        for vb1 in verbs1:
+            syns1 = wn.synsets(vb1)
+            for vb2 in verbs2:
+                d = nltk.edit_distance(vb1, vb2)
+                syns2 = wn.synsets(vb2)
+                s = syns1[0].wup_similarity(syns2[0])
+                #s = syns1[0].path_similarity(syns2[0])
+                if s > 0.2 and s < 0.4:
+                    verbWeight += 0.4
+                elif s >= 0.4 and s < 0.75:
+                    verbWeight += s
+                elif s >= 0.75 and s <= 1:
+                    verbWeight += 1
+                else:
+                    if s:
+                        verbWeight += s
+                
+                # If distance is lesst than or equal 2 then add 0.2
+                if d <= 2:  
+                    verbWeight += 0.2
+                
+    '''         
+    print syns1
+    print syns2
+    '''
+    
+    outputWeight = nounWeight + verbWeight
+    print "Output"
+    '''
+    if outputWeight > 1:
+        return 1
+    '''
+    return outputWeight
+
+if __name__ == "__main__":
+    print semantic_similarity('amateur crossdresser','amateur gang bang')
+    print semantic_similarity('world war','the great war')
+
---
+++ db.py
@@ -0,0 +1,94 @@
+import math
+import nltk.collocations
+import nltk.corpus
+import collections
+from nltk.corpus import wordnet as wn
+from slugify import slugify
+from arango import ArangoClient
+from bk import semantic_similarity
+
+client = ArangoClient(
+    protocol='http',
+    host='localhost',
+    port=8529,
+    username='root',
+    password='root',
+    enable_logging=True
+)
+'''
+db = client.create_database('yahoo_query')
+keywords = db.create_collection('keywords')
+keywords.add_hash_index(fields=['name'], unique=True)
+'''
+db = client.db('yahoo_query')
+
+f = open('ydata.txt')
+rawText = f.read()
+splitText = rawText.split('\n')
+
+
+def convertToKey(s):
+    ord3 = lambda x : '%.3d' % ord(x)
+    return int(''.join(map(ord3, s)))
+
+def convertFromKey(n):
+    s = str(n)
+    return ''.join([chr(int(s[i:i+3])) for i in range(0, len(s), 3)])
+
+def create_nodes(stxt):
+    keywords = db.create_collection('keywords')
+    for txt in stxt:        
+        if txt:
+            txt = txt.strip().strip('"')
+            key = convertToKey(txt)
+            
+            data = {'_key': str(key), 'name': txt, 'title': txt}
+            keywords.insert(data)       
+            print data
+        
+    return True
+        
+
+def create_relation_graph(stxt):
+    
+    count = 0   
+    countCompare = 0   
+    
+    
+    relation_graph = db.create_graph('relation')
+    relations = relation_graph.create_edge_definition(
+        name='relations',
+        from_collections=['keywords'],
+        to_collections=['keywords']
+    )
+    
+    
+    for txt in stxt:        
+        
+        txt = txt.strip().strip('"')
+                
+        
+        for compareTxt in stxt:            
+            '''
+            if countCompare > 5000:
+                break
+            '''
+            compareTxt = compareTxt.strip().strip('"')
+            s = semantic_similarity(txt,compareTxt)
+            
+            if s >= 1:
+                keyTxt = convertToKey(txt)
+                keycompareTxt = convertToKey(compareTxt)
+                data = {'_from': 'keywords/' + str(keyTxt), '_to': 'keywords/'+ str(keycompareTxt) }               
+                relations.insert(data)
+                print data
+     
+            countCompare += 1
+    
+        count += 1
+    
+    
+createNodes = create_nodes(splitText)
+
+if createNodes:
+    create_relation_graph(splitText)
